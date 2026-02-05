e9045a2f6b3855ab6608b3fe5981f93960dbe31b
Luís Paulo da Silva <luisp.software.development@gmail.com>
2026-02-05 15:09:26 -0300

    Adding controller tests

--- src/main/java/com/luispdev/javaservicetemplate/post/PostExceptionHandler.java
+++ src/main/java/com/luispdev/javaservicetemplate/post/PostExceptionHandler.java
@@ -1,17 +1,16 @@
 package com.luispdev.javaservicetemplate.post;
 
 import com.luispdev.javaservicetemplate.post.exception.PostNotFoundException;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class PostExceptionHandler {
 
-    @ResponseStatus(value = HttpStatus.NOT_FOUND)
     @ExceptionHandler(PostNotFoundException.class)
-    public void notFound() {
+    public ResponseEntity<String> notFound() {
+        return ResponseEntity.notFound().build();
     }
 
 }
